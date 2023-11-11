888164a4b45efa4237fdd4fdf017b946c468272b
Vilmen Abramian <abramian.vl@phystech.edu>
2023-11-11 18:32:44 +0300

    Made admin page

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -7,29 +7,58 @@ User = get_user_model()
 
 
 class Category(BaseModel): # Тематическая категория
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=256, verbose_name = 'Заголовок')
+    description = models.TextField(verbose_name = 'Описание')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name = 'Идентификатор',
+        help_text = 'Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 class Location(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name = 'Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
 
 class Post(BaseModel): # Публикация
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=256, verbose_name = 'Заголовок')
+    text = models.TextField(verbose_name = 'Текст')
+    pub_date = models.DateTimeField(
+        verbose_name = 'Дата и время публикации',
+        help_text = 'Если установить дату и время в будущем — можно делать отложенные публикации.')
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE,
+        verbose_name = 'Автор публикации')
     location = models.ForeignKey(
         Location,
         null=True,
         on_delete=models.SET_NULL,
-        blank=True
+        blank=True,
+        verbose_name = 'Местоположение'
     )
     category = models.ForeignKey(
         Category,
         null=True,
         on_delete=models.SET_NULL,
+        verbose_name = 'Категория'
     )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
 
--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    is_published = models.BooleanField(default=True, verbose_name = 'Опубликовано')
-    created_at = models.DateTimeField()
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name = 'Опубликовано',
+        help_text = 'Снимите галочку, чтобы скрыть публикацию.')
+    created_at = models.DateTimeField(verbose_name = 'Добавлено')
 
     class Meta():
         abstract = True
